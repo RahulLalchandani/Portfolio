9f24e143c68499fdbd4723d8f94128e4a33399c9
RahulLalchandani <7864.lalchandani@gmail.com>
2022-08-28 05:16:36 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+<script>
 const element = <h1>Rahul Lalchandani</h1>;
 ReactDOM.render(element,document.getElementById('root'));
+</script>
